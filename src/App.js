9d448d9faf264036812b33b8960382fb06120c18
uladzimir <01myit@gmail.com>
2023-08-31 21:06:23 +0300

    fixed bug about unique keys

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState, Fragment } from "react"
 import TaskList from "./components/Tasks/TaskList/TaskList"
 import TaskInput from "./components/Tasks/TaskInput/TaskInput"
+import {v4} from 'uuid'
 import "./App.css"
 
 const App = () => {
   const [tasks, setTasks] = useState([
-    { text: "Создание курса - 1 час", id: "t1" },
-    { text: "Разминка 15 мин", id: "t2" },
-    { text: "Создание курса - 1 час", id: "t3" },
+    { text: "Создание курса - 1 час", id: v4() },
+    { text: "Разминка 15 мин", id: v4() },
+    { text: "Создание курса - 1 час", id: v4() },
   ]) 
 
   const addTaskHandler = (inputText) => {
     setTasks((prevTasks) => {
       const updatedTasks = [...prevTasks]
-      updatedTasks.unshift({ text: inputText, id: 'task1' })
+      updatedTasks.unshift({ text: inputText, id: v4() })
       return updatedTasks
     })
   }
